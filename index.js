65a4744628336ed93af89675d598622272f1c68d
Raquel Moss <raquelxmoss@gmail.com>
2020-04-21 13:38:10 +0200

    hmm

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const makeHttpRequest = (event) => {
+  console.log(event)
+  console.log(event.key)
   axios.post("https://dummy.restapiexample.com/api/v1/create", {event})
 }
+
+console.log('document', document)
+console.log('window', window)
 
 document.addEventListener('keyup', makeHttpRequest);

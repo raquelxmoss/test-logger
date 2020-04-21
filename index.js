c594a1458425f55a267571447ab73dafb982f5b2
Raquel Moss <raquelxmoss@gmail.com>
2020-04-21 13:40:08 +0200

    asdf

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const makeHttpRequest = (event) => {
   console.log(event)
   console.log(event.key)
-  axios.post("https://dummy.restapiexample.com/api/v1/create", {event})
+  axios.post("https://dummy.restapiexample.com/api/v1/create", {key: event.key})
 }
 
 console.log('document', document)
